650084ea07bacd61d782b679f811f2acef252e33
Matthew <156413734+mehdi-tahan@users.noreply.github.com>
2025-08-13 10:22:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("bgVideo");
+  if (window.innerWidth > 768) {
+    video.src = "assets/videos/forest_pc.mp4";
+  } else {
+    video.src = "assets/videos/forest_phone.mp4";
+  }
+});
 // Custom JS can go here
